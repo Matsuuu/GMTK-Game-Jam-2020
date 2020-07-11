d0817576f4be583880724b6b92c896268219ea73
Matsuuu <huhta.matias@gmail.com>
2020-07-11 22:50:49 +0300

    traps

---
+++ GMTK-Game-Jam-2020/Assets/Scripts/BallNChain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallNChain : MonoBehaviour
+{
+    public float animationSpeed;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = animationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
