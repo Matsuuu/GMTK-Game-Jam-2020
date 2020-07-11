ab44415d9786e06de55893b811d922c75b07d96b
Matsuuu <huhta.matias@gmail.com>
2020-07-11 19:05:33 +0300

    Goal post

---
+++ GMTK-Game-Jam-2020/Assets/GoalZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone : MonoBehaviour
+{
+    private PersistentDataManager persistentDataManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        persistentDataManager = GameObject.Find("PersistentDataManager").GetComponent<PersistentDataManager>();
+        persistentDataManager.StartTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        persistentDataManager.EndTime();
+    }
+}
---
+++ GMTK-Game-Jam-2020/Assets/Scripts/PersistentDataManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentDataManager : MonoBehaviour
+{
+    public struct StageTime
+    {
+        public  int minutes { get; set; }
+        public int seconds { get; set; }
+        public StageTime(int minutes, int seconds)
+        {
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+    }
+    public int currentStage = 1;
+
+    public StageTime stageTime;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTime()
+    {
+        startTime = Time.time;
+    }
+
+    public void EndTime()
+    {
+        float timeTaken = Time.time - startTime;
+
+        int minutes = (int) Mathf.Floor(timeTaken / 60);
+        int seconds = (int)timeTaken % 60;
+        stageTime = new StageTime(minutes, seconds);
+        Debug.Log("Finished with time: " + minutes + " minutes, " + seconds + " seconds.");
+    }
+}
