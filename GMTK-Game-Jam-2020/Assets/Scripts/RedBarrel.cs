eec311ee3d6f2ab915bb2e6d1600dc4bf27b05d7
Pakkanen1 <bonetailur@gmail.com>
2020-07-12 15:20:11 +0300

    exploding barrels

---
+++ GMTK-Game-Jam-2020/Assets/Scripts/RedBarrel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RedBarrel : MonoBehaviour
+{
+    private AudioSource barrelAudioSource;
+    public AudioClip explodeSound;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrelAudioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Destroy(GetComponent<Rigidbody2D>());
+            barrelAudioSource.clip = explodeSound;
+            StartCoroutine(ExplodeBarrel());
+        }
+    }
+
+    public IEnumerator ExplodeBarrel()
+    {
+        barrelAudioSource.Play();
+        animator.SetTrigger("Explosion");
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
