a9d854053a3fc1504f07b3f8cb9be86dd0485643
Pakkanen1 <bonetailur@gmail.com>
2020-07-12 17:02:25 +0300

    fogiiii

---
+++ GMTK-Game-Jam-2020/Assets/Scripts/Fog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    public float moveSpeed;
+    public int fogWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+    
+    private void OnBecameInvisible()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + fogWidth * 2, pos.y, pos.z);
+    }
+}
