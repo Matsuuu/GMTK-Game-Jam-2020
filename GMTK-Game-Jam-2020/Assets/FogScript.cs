a852102a3b4130d83c89bb1820eb71d204e50198
Matsuuu <huhta.matias@gmail.com>
2020-07-12 18:08:03 +0300

    Stage

---
+++ GMTK-Game-Jam-2020/Assets/FogScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogScript : MonoBehaviour
+{
+    
+    public float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 260);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+    }
+}
--- GMTK-Game-Jam-2020/Assets/Scripts/FogSpammer.cs
+++ GMTK-Game-Jam-2020/Assets/Scripts/FogSpammer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FogSpammer : MonoBehaviour
 {
+    public GameObject fog;
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(SpawnFog());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnFog()
+    {
+        while (true)
+        {
+            GameObject f = GameObject.Instantiate(fog, transform.position, transform.rotation);
+            yield return new WaitForSeconds(20);
+        }
     }
 }
